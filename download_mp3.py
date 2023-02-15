6d1c26027a3a73b8b73223ea268a8a3262192fe1
Pier Paolo Tarasco <pier.tarasco@gmail.com>
2023-02-15 17:15:33 +0100

    update urls

--- download_mp3.py
+++ download_mp3.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
 
 url = "https://www.studiotamani.org/125169-les-titres-du-17-janvier-2023-soir"
 
 # Download the web page
 response = requests.get(url)
 content = response.content
+
+folder = "original"
 
 # Use BeautifulSoup to parse the HTML
 soup = BeautifulSoup(content, 'html.parser')
@@ -20,6 +23,10 @@ for link in links:
     mp3_response = requests.get(mp3_url)
     mp3_content = mp3_response.content
     filename = mp3_url.split("/")[-1]
+    filename = f"{folder}/{filename}"
+    # Create the folder if it doesn't exist
+    if not os.path.exists(folder):
+        os.mkdir(folder)
     with open(filename, "wb") as f:
         f.write(mp3_content)
     print(f"Downloaded {filename}")
