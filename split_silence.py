f8a984224bcb7125175ba615f31cfd8d097e99b5
Pier Paolo Tarasco <pier.tarasco@gmail.com>
2023-02-16 10:03:00 +0100

    30 seconds

--- split_silence.py
+++ split_silence.py
@@ -6,8 +6,8 @@ import pandas as pd
 # Set the input file
 input_file = "original/Journal_tas-17012023.mp3"
 db = 30
-duration = 0.2
-output_dir = "silence_7seconds"
+duration = 0.35
+output_dir = "silence_30seconds"
 
 # Check if the output directory exists, if not create it; otherwise ask the user if they want to overwrite the directory
 try:
@@ -68,6 +68,7 @@ for i in range(len(start_times)):
     results["End Time"].append(real_end_times[i])
     results["Duration (sec)"].append(audio_durations[i])
     url = f"https://github.com/Pier297/tamasheq/raw/main/{output_dir}/output_{i}.mp3"
+    #url = f"{output_dir}/output_{i}.mp3"
     hyperlink = f'=HYPERLINK("{url}", "{output_dir}/output_{i}.mp3")'
     results["Link"].append(hyperlink)
 
